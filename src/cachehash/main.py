b254af5b4579a10a6ac3f9f5fa523f53de4f28e5
Jason Nichols <github@ayl.us>
2025-03-07 13:14:10 -0500

    Update main.py

--- src/cachehash/main.py
+++ src/cachehash/main.py
@@ -128,7 +128,7 @@ class Cache:
         hash = self.get_hash(file_path)
         row = self.query(
             "get_record",
-            {"hash": hash},           },
+            {"hash": hash},
         ).fetchone()
 
         if row is None:
